d9778c5362b87d70c7454f66758eb045301eddb0
mg <>
2005-11-14 10:01:19 +0000

    Updated to (at least) work with the new athena API.
    I'm really not convinced this is quite correct but it does work. In particular
    I feel that the UI-ness of the Calculator class is mixed up with the model,
    but then it's only supposed to be a simple example!

--- examples/calculator/calculator.py
+++ examples/calculator/calculator.py
@@ -1,64 +1,56 @@
-from zope.interface import implements
+from zope.interface import implements, Interface
+from twisted.python.components import registerAdapter
 from nevow import athena, inevow, loaders, tags, util
 
+class ICalculator(Interface):
+    def buttonClicked(button):
+        pass
 
-class Calculator(athena.LivePage):
-    """
-    A "live" calculator.
-
-    All buttons presses in the browser are sent to the server. The server
-    evaluates the expression and sets the output in the browser.
-    """
-
-    docFactory = loaders.xmlfile(
-        util.resource_filename('calculator', 'calculator.html')
-        )
+class Calculator(object):
 
     validSymbols = '0123456789/*-=+.C'
     defaultExpression = u'0'
     errorExpression = u'E'
-    
-    def __init__(self, *a, **kw):
-        super(Calculator, self).__init__(*a, **kw)
+
+    def __init__(self):
         self.expression = self.defaultExpression
-    
-    def child_(self, ctx):
-        return self
-        
-    def remote_buttonClicked(self, ctx, symbol):      
-        """
-        Calculator button pressed.
-        """
+
+    def buttonClicked(self, symbol):
         # Remember ... never trust a browser
         if symbol not in self.validSymbols:
             raise ValueError('Invalid symbol')
         # Clear
         if symbol == 'C':
-            self.expression = self.defaultExpression  
-            return self.updateOutput()
+            self.expression = self.defaultExpression
+            return self.expression
         # Check the expression is currently valid
         if self.expression == self.errorExpression:
-            return
+            return self.expression
         # Evaluate the expression
         if symbol == '=':
             try:
                 self.expression = unicode(eval(self.expression))
             except ZeroDivisionError:
                 self.expression = self.errorExpression
-            return self.updateOutput()
+            return self.expression
         # Replace of add to the expression
         if self.expression == self.defaultExpression:
             self.expression = symbol
         else:
             self.expression += symbol
-        return self.updateOutput()
+        return self.expression
 
-    def updateOutput(self):
-        """
-        Send the current expression to the browser.
-        """
-        return self.callRemote('setOutput', self.expression)
+class CalculatorResource(athena.LivePage):
+    """
+    A "live" calculator.
 
+    All buttons presses in the browser are sent to the server. The server
+    evaluates the expression and sets the output in the browser.
+    """
+    addSlash = True
+    docFactory = loaders.xmlfile(
+        util.resource_filename('calculator', 'calculator.html')
+        )
 
 if __name__ == '__main__':
 
@@ -67,21 +59,13 @@ if __name__ == '__main__':
     from twisted.python import log
     from nevow import appserver
 
-    class Root(object):
-        """
-        Strange root resource that only exists to create a Calculator
-        LivePage for each request.
-        """
-        implements(inevow.IResource)
-        calculatorFactory = athena.LivePageFactory(Calculator)
-        def locateChild(self, ctx, segments):
-            return self.makeCalculator(ctx), segments
-        def renderHTTP(self, ctx):
-            return self.makeCalculator(ctx)
-        def makeCalculator(self, ctx):
-            return self.calculatorFactory.clientFactory(ctx)
+    def calculatorResourceFactory(original):
+        return CalculatorResource(ICalculator, original)
+
+    registerAdapter(calculatorResourceFactory, Calculator, inevow.IResource)
 
     log.startLogging(sys.stdout)
-    site = appserver.NevowSite(Root())
+    calculator = Calculator()
+    site = appserver.NevowSite(calculator)
     reactor.listenTCP(8080, site)
     reactor.run()
