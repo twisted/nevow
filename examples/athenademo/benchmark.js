537d7806d5f799abc810027fa7f48373e157a8d1
exarkun <>
2006-04-07 13:46:06 +0000

    benchmark Divmod.Runtime.Platform.evaluate()

--- examples/athenademo/benchmark.js
+++ examples/athenademo/benchmark.js
@@ -12,26 +12,72 @@ document.documentElement.insertBefore(
     document.createTextNode(timer()),
     document.documentElement.firstChild);
 
-Nevow.Benchmarks.InitializationBenchmark = Nevow.Athena.Widget.subclass('Nevow.Benchmarks.InitializationBenchmark');
-Nevow.Benchmarks.InitializationBenchmark.methods(
-    function __init__(self, node) {
-	Nevow.Benchmarks.InitializationBenchmark.upcall(self, '__init__', node);
-	self.stamptime();
-	self.colorize('yellow');
+Nevow.Benchmarks.BenchmarkBase = Nevow.Athena.Widget.subclass('Nevow.Benchmarks.BenchmarkBase');
+Nevow.Benchmarks.BenchmarkBase.methods(
+    function starttime(self) {
+        self._starttime = new Date();
     },
 
-    function loaded(self) {
-	self.stamptime();
-	self.colorize('purple');
-	var d = self.callRemote('activate');
-	d.addCallback(function() { self.stamptime(); self.colorize('green'); });
-	d.addErrback(function() { self.stamptime(); self.colorize('red'); });
-    },
-
-    function stamptime(self) {
-	self.node.appendChild(document.createTextNode(' ' + timer()));
+    function endtime(self) {
+        var endtime = new Date();
+        var starttime = self._starttime;
+        delete self._starttime;
+        Divmod.Runtime.theRuntime.setNodeContent(
+            self.nodeByAttribute('class', 'timing-result'),
+            '<span xmlns="http://www.w3.org/1999/xhtml">Seconds Elapsed: ' + ((endtime - starttime) / 1000.0) + "</span>");
     },
 
     function colorize(self, color) {
 	self.node.style.color = color;
+    });
+
+
+Nevow.Benchmarks.InitializationBenchmark = Nevow.Benchmarks.BenchmarkBase.subclass('Nevow.Benchmarks.InitializationBenchmark');
+Nevow.Benchmarks.InitializationBenchmark.methods(
+    function __init__(self, node) {
+	Nevow.Benchmarks.InitializationBenchmark.upcall(self, '__init__', node);
+	self.starttime();
+	self.colorize('yellow');
+	var d = self.callRemote('activate');
+	d.addCallback(function() { self.endtime(); self.colorize('green'); });
+	d.addErrback(function() { self.endtime(); self.colorize('red'); });
+    });
+
+
+Nevow.Benchmarks.EvaluateBenchmark = Nevow.Benchmarks.BenchmarkBase.subclass('Nevow.Benchmarks.EvaluateBenchmark');
+Nevow.Benchmarks.EvaluateBenchmark.methods(
+    function __init__(self, node) {
+        Nevow.Benchmarks.EvaluateBenchmark.upcall(self, '__init__', node);
+    },
+
+    function _evaluateRepeatedly(self, expr, times) {
+        var d = Divmod.Defer.Deferred();
+        function evaluateOnce() {
+            // Divmod.Runtime.theRuntime.evaluate(expr);
+            eval(expr);
+            if (--times >= 0) {
+                setTimeout(evaluateOnce, 0);
+            } else {
+                d.callback(null);
+            }
+        };
+        evaluateOnce();
+        return d;
+    },
+
+    function evaluateSomeThings(self) {
+        var a = new Array();
+        for (var i = 0; i < 1000; ++i) {
+            a.push(["hello", {world: "how"}, "are", "you", "d", 0.1, "ng"]);
+        }
+        var expr = a.toSource();
+        self.starttime();
+        self.colorize('yellow');
+        self._evaluateRepeatedly(expr, 100).addCallback(function() {
+            self.endtime();
+            self.colorize('green');
+        }).addErrback(function() {
+            self.endtime();
+            self.colorize('red');
+        });
     });
--- examples/athenademo/benchmark.py
+++ examples/athenademo/benchmark.py
@@ -5,6 +5,44 @@ from twisted.python import filepath
 
 from nevow import tags, loaders, athena
 
+
+class InitializationBenchmark(athena.LiveFragment):
+    jsClass = u'Nevow.Benchmarks.InitializationBenchmark'
+
+    docFactory = loaders.stan(
+        tags.div(render=tags.directive('liveFragment'))[
+            "Initialization Benchmark",
+            " ",
+            tags.span(class_='timing-result')])
+
+    def __init__(self, page):
+        super(InitializationBenchmark, self).__init__()
+        self.setFragmentParent(page)
+
+
+    def activate(self):
+        pass
+    athena.expose(activate)
+
+
+
+class EvaluateBenchmark(athena.LiveFragment):
+    jsClass = u'Nevow.Benchmarks.EvaluateBenchmark'
+
+    docFactory = loaders.stan(
+        tags.div(render=tags.directive('liveFragment'))[
+            "Divmod.Runtime.Platform.evaluate() Benchmark",
+            tags.button[athena.handler(event='onclick', handler='evaluateSomeThings'),
+                        "Evaluate"],
+            " ",
+            tags.span(class_='timing-result')])
+
+    def __init__(self, page):
+        super(EvaluateBenchmark, self).__init__()
+        self.setFragmentParent(page)
+
+
+
 class Benchmark(athena.LivePage):
     docFactory = loaders.stan(tags.html[
         tags.head(render=tags.directive('liveglue')),
@@ -21,6 +59,7 @@ class Benchmark(athena.LivePage):
             'Nevow.Benchmarks': here.path})
 
     def render_body(self, ctx, data):
+        outermost = tags.div(style='border: solid thin black;')
         for j in range(self.maxNodes // self.maxDepth):
             top = t = tags.div()
             for i in range(self.maxDepth):
@@ -28,20 +67,7 @@ class Benchmark(athena.LivePage):
                 t[m]
                 t = m
             t[InitializationBenchmark(self)]
-            yield top
+            outermost[top]
+        yield outermost
 
-class InitializationBenchmark(athena.LiveFragment):
-    jsClass = u'Nevow.Benchmarks.InitializationBenchmark'
-
-    docFactory = loaders.stan(
-        tags.div(render=tags.directive('liveFragment'))[
-            "Initialization Benchmark"])
-
-    def __init__(self, page):
-        super(InitializationBenchmark, self).__init__()
-        self.page = page
-
-
-    def activate(self):
-        pass
-    athena.expose(activate)
+        yield tags.div(style='border: solid thin black;')[EvaluateBenchmark(self)]
