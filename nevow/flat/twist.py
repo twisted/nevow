84edac6e5374c03253b33eeb16095388e6c8001d
exarkun <>
2006-05-23 18:21:19 +0000

    Merge nevow-memfprint-832
    Author: exarkun
    Reviewer: moea
    Fixes #832
    
    Implement suggestion from john@squishy.net to remove the nested function in
    the Deferred support of flattening.  This removes a cycle and should relieve
    some memory pressure.  This probably also speeds up rendering a miniscule
    amount by removing a couple nested function definitions.

--- nevow/flat/twist.py
+++ nevow/flat/twist.py
@@ -2,44 +2,54 @@
 # See LICENSE for details.
 
 from twisted.python.context import get as getCtx
-from nevow.util import Deferred
+from twisted.internet import defer
 from nevow import flat
 
 
-def deferflatten(stan, ctx, writer):
-    finished = Deferred()
+def _isDeferred(d):
+    """
+    Flattener predicate for trampolining out when handling a Deferred.
+    """
+    return isinstance(d, defer.Deferred)
 
-    iterable = flat.iterflatten(
-        stan, ctx, writer, lambda D: isinstance(D, Deferred))
 
-    def drive():
-        try:
-            next = iterable.next()
-        except StopIteration:
-            finished.callback('')
-        except:
-            finished.errback()
+def _drive(iterable, finished):
+    """
+    Spin an iterable returned by L{nevow.flat.iterflatten}, setting up
+    callbacks and errbacks on Deferreds it spits out so as to continue spinning
+    it after those Deferreds fire.
+    """
+    try:
+        next = iterable.next()
+    except StopIteration:
+        finished.callback('')
+    except:
+        finished.errback()
+    else:
+        deferred, returner = next
+        def cb(result):
+            returner(result)
+            _drive(iterable, finished)
+            return result
+        def eb(failure):
+            finished.errback(failure)
+            return failure
+        cfac = getCtx('CursorFactory')
+        if cfac:
+            deferred.addCallback(cfac.store.transback, cb).addErrback(cfac.store.transback, eb)
         else:
-            deferred, returner = next
-            def cb(result):
-                returner(result)
-                drive()
-                return result
-            def eb(failure):
-                finished.errback(failure)
-                return failure
-            cfac = getCtx('CursorFactory')
-            if cfac:
-                deferred.addCallback(cfac.store.transback, cb).addErrback(cfac.store.transback, eb)
-            else:
-                deferred.addCallback(cb).addErrback(eb)
+            deferred.addCallback(cb).addErrback(eb)
 
-    drive()
+
+def deferflatten(stan, ctx, writer):
+    finished = defer.Deferred()
+    iterable = flat.iterflatten(stan, ctx, writer, _isDeferred)
+    _drive(iterable, finished)
     return finished
-    
+
 
 def DeferredSerializer(original, context):
-    d = Deferred()
+    d = defer.Deferred()
     def cb(result):
         d.callback(flat.serialize(result, context))
         return result
