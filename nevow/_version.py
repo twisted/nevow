4b06cd1c471c2f2d0ad66d69c1bd95311bf273bc
washort <>
2006-10-17 21:17:10 +0000

    merge release-20061017

--- nevow/_version.py
+++ nevow/_version.py
@@ -1,3 +1,3 @@
 # This is an auto-generated file. Use admin/change-versions to update.
 from twisted.python import versions
-version = versions.Version(__name__[:__name__.rfind('.')], 0, 9, 11)
+version = versions.Version(__name__[:__name__.rfind('.')], 0, 9, 12)
