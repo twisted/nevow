7bab3ae9947a56dc0a1fd0591ae7edacf5eb8a6f
exarkun <>
2007-02-13 19:11:03 +0000

    Merge remove-runjstests-1951
    Author: exarkun
    Reviewer: washort
    Fixes #1951
    
    Delete the redundant runjstests script and support modules.  JavaScript unit
    tests can be run with trial now.

--- nevow/testutil.py
+++ nevow/testutil.py
@@ -3,7 +3,6 @@
 
 import sys, signal
 from popen2 import popen2
-from StringIO import StringIO
 
 from zope.interface import implements
 
@@ -12,15 +11,18 @@ try:
 except ImportError:
     subunit = None
 
+from twisted.python import procutils
+from twisted.python.util import sibpath
 from twisted.trial.unittest import TestCase as TrialTestCase
 from twisted.python.components import Componentized
 from twisted.internet import defer
 from twisted.web import http
+from twisted.protocols.basic import LineReceiver
 
 from formless import iformless
 
+import nevow
 from nevow import inevow, context, athena, loaders, tags, appserver
-from nevow.scripts import consolejstest
 
 
 class FakeChannel:
@@ -299,6 +301,136 @@ class NotSupported(Exception):
     """
 
 
+_DUMMY_MODULE_NAME = 'ConsoleJSTest'
+
+def getDependencies(fname, ignore=('MochiKit.DOM',),
+                    bootstrap=athena.LivePage.BOOTSTRAP_MODULES,
+                    packages=None):
+    """
+    Get the javascript modules that the code in the file with name C{fname}
+    depends on, recursively
+
+    @param fname: javascript source file name
+    @type fname: C{str}
+
+    @param ignore: names of javascript modules to exclude from dependency list
+    @type ignore: sequence
+
+    @param boostrap: names of javascript modules to always include, regardless
+    of explicit dependencies (defaults to L{athena.LivePage}'s list of
+    bootstrap modules)
+    @type boostrap: sequence
+
+    @param packages: all javascript packages we know about.  defaults to the
+    result of L{athena.allJavascriptPackages}
+    @type packages: C{dict}
+
+    @return: modules included by javascript in file named C{fname}
+    @rtype: dependency-ordered list of L{athena.JSModule} instances
+    """
+    if packages is None:
+        packages = athena.allJavascriptPackages()
+
+    # TODO if a module is ignored, we should ignore its dependencies
+    bootstrapModules = [athena.JSModule.getOrCreate(m, packages)
+                        for m in bootstrap if m not in ignore]
+
+    packages[_DUMMY_MODULE_NAME] = fname
+    module = athena.JSModule(_DUMMY_MODULE_NAME, packages)
+
+    return (bootstrapModules +
+            [dep for dep in module.allDependencies()
+             if (dep.name not in bootstrap
+                 and dep.name != _DUMMY_MODULE_NAME
+                 and dep.name not in ignore)])
+
+
+
+def findJavascriptInterpreter():
+    """
+    Return a string path to a JavaScript interpreter if one can be found in
+    the executable path. If not, return None.
+    """
+    for script in ['smjs', 'js']:
+        _jsInterps = procutils.which(script)
+        if _jsInterps:
+            return _jsInterps[0]
+    return None
+
+
+
+def generateTestScript(fname, after={'Divmod.Base': ('Divmod.Base.addLoadEvent = function() {};',)},
+                              dependencies=None):
+    """
+    Turn the contents of the Athena-style javascript test module in the file
+    named C{fname} into a plain javascript script.  Recursively includes any
+    modules that are depended on, as well as the utility module
+    nevow/test/testsupport.js.
+
+    @param fname: javascript source file name
+    @type fname: C{str}
+
+    @param after: mapping of javascript module names to sequences of lines of
+    javascript source that should be injected into the output immediately
+    after the source of the named module is included
+    @type after: C{dict}
+
+    @param dependencies: the modules the script depends on.  Defaults to the
+    result of L{getDependencies}
+    @type dependencies: dependency-ordered list of L{athena.JSModule}
+    instances
+
+    @return: converted javascript source text
+    @rtype: C{str}
+    """
+    if dependencies is None:
+        dependencies= getDependencies(fname)
+
+    load = lambda fname: 'load(%r);' % (fname,)
+    initialized = set()
+    js = [load(sibpath(nevow.__file__, 'test/testsupport.js'))]
+    for m in dependencies:
+        segments = m.name.split('.')
+        if segments[-1] == '__init__':
+            segments = segments[:-1]
+        initname = '.'.join(segments)
+        if initname not in initialized:
+            initialized.add(initname)
+            if '.' in initname:
+                prefix = ''
+            else:
+                prefix = 'var '
+            js.append('%s%s = {};' % (prefix, initname))
+        js.append(load(m.mapping[m.name]))
+        if m.name in after:
+            js.extend(after[m.name])
+
+    js.append(file(fname).read())
+
+    return '\n'.join(js)
+
+
+
+class TestProtocolLineReceiverServer(LineReceiver):
+    """
+    Subunit protocol which is also a Twisted LineReceiver so that it
+    includes line buffering logic.
+    """
+    delimiter = '\n'
+
+    def __init__(self, proto):
+        self.proto = proto
+
+
+    def lineReceived(self, line):
+        """
+        Forward the line on to the subunit protocol's lineReceived method,
+        which expects it to be newline terminated.
+        """
+        self.proto.lineReceived(line + '\n')
+
+
+
 class JavaScriptTestCase(TrialTestCase):
     def __init__(self, methodName='runTest'):
         TrialTestCase.__init__(self, methodName)
@@ -311,7 +443,7 @@ class JavaScriptTestCase(TrialTestCase):
 
         @raise NotSupported: If any one of the dependencies is not satisfied.
         """
-        js = consolejstest.findJavascriptInterpreter()
+        js = findJavascriptInterpreter()
         if js is None:
             raise NotSupported("Could not find JavaScript interpreter")
         if subunit is None:
@@ -336,7 +468,7 @@ class JavaScriptTestCase(TrialTestCase):
 Divmod.UnitTest.runRemote(Divmod.UnitTest.loadFromModule(%(module)s));
 """ % {'module': testModule}
         jsfile = self._writeToTemp(js)
-        scriptFile = self._writeToTemp(consolejstest.generateTestScript(jsfile))
+        scriptFile = self._writeToTemp(generateTestScript(jsfile))
         return scriptFile
 
 
@@ -359,23 +491,26 @@ Divmod.UnitTest.runRemote(Divmod.UnitTest.loadFromModule(%(module)s));
             result.addSkip(self, str(e))
             result.stopTest(self)
             return
-        js = consolejstest.findJavascriptInterpreter()
+        js = findJavascriptInterpreter()
         script = self.makeScript(self.testMethod())
-        protocol = subunit.TestProtocolServer(result)
+        server = subunit.TestProtocolServer(result)
+        protocol = TestProtocolLineReceiverServer(server)
 
         # What this *SHOULD BE*
-        # d = getProcessOutput(js, (script,))
-        # d.addCallback(StringIO)
-        # d.addCallback(protocol.readFrom)
-        # return d
+        # spawnProcess(protocol, js, (script,))
+        # return protocol.someDisconnectCallback()
 
         # However, *run cannot return a Deferred profanity profanity profanity
         # profanity*, so instead it is *profanity* this:
         def run():
             r, w = popen2([js, script])
-            return r.read()
-        output = self._runWithSigchild(run)
-        protocol.readFrom(StringIO(output))
+            while True:
+                bytes = r.read(4096)
+                if bytes:
+                    protocol.dataReceived(bytes)
+                else:
+                    break
+        self._runWithSigchild(run)
 
 
 
@@ -386,7 +521,7 @@ def setJavascriptInterpreterOrSkip(testCase):
     assign the path to the interpreter executable to
     C{testCase.javascriptInterpreter}
     """
-    script = consolejstest.findJavascriptInterpreter()
+    script = findJavascriptInterpreter()
     if script is None:
         testCase.skip = "No JavaScript interpreter available."
     else:
