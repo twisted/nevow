91d691fcf1652aabdb3f57f2a5787bf3acbfb706
exarkun <>
2008-05-07 18:10:20 +0000

    Merge undeprecated-athena-widget-2588
    Author: exarkun
    Reviewer: pjd
    Fixes: #2588
    
    If available, use the public API introduced in Twisted 8.0.1 for declaring the
    twistd plugin for viewing athena widgets.

--- twisted/plugins/nevow_widget.py
+++ twisted/plugins/nevow_widget.py
@@ -4,13 +4,20 @@
 twistd subcommand plugin for launching an athena widget server.
 """
 
-from twisted.scripts.mktap import _tapHelper
+try:
+    # Twisted 8.0.1 (r23252, to be precise) introduced a public API for
+    # this.
+    from twisted.application.service import ServiceMaker
+except ImportError:
+    # For versions of Twisted older than that, fallback to the private
+    # version of the same thing.
+    from twisted.scripts.mktap import _tapHelper as ServiceMaker
 
-widgetServiceMaker = _tapHelper(
-    "Widget Mathingwhathuh",
+widgetServiceMaker = ServiceMaker(
+    "Stand-alone Athena Widget runner",
     "nevow._widget_plugin",
     """
     Create a service which starts a NevowSite with a single page with a single
-    widget.
+    athena widget.
     """,
     "athena-widget")
