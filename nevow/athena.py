fd9bdf13434027aac4bdd15350dbd2d6cca16feb
Logan Perkins <perkins@github.alestan.publicvm.com>
2016-02-11 01:45:26 -0800

    more stupid unicode issues

--- nevow/athena.py
+++ nevow/athena.py
@@ -1346,7 +1346,7 @@ class LivePage(rend.Page, _HasJSClass, _HasCSSModule):
 
 
     def getJSModuleURL(self, moduleName):
-        print(1349, moduleName)
+        print(1349, self.jsModuleRoot.child(moduleName), flat.flatten(self.jsModuleRoot.child(moduleName)))
         return self.jsModuleRoot.child(moduleName)
 
 
